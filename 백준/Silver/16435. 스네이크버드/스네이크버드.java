e299e335c19673893541467a08bfacbb31ae7a00
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-18 20:58:06 +0900

    [Silver V] Title: 스네이크버드, Time: 132 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int []H = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            H[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(H);
+        for(int i=0;i<N;i++){
+            if(H[i] <= L) L++;
+        }
+        System.out.println(L);
+    }
+}
