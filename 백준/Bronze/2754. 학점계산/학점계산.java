35630d10895459b38abbe900a671bd28bbc0c03e
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-19 15:25:22 +0900

    [Bronze III] Title: 학점계산, Time: 140 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        double grade = 0.0;
+        char alph = str.charAt(0);
+        char flag = str.length()>1 ? str.charAt(1): '0';
+        switch (alph){
+            case 'A':
+                grade = 4.0;
+                break;
+            case 'B':
+                grade = 3.0;
+                break;
+            case 'C':
+                grade = 2.0;
+                break;
+            case 'D':
+                grade = 1.0;
+                break;
+            default:
+                grade = 0.0;
+        }
+        if(flag == '+') grade+=0.3;
+        else if(flag == '-') grade-=0.3;
+
+        bw.write(String.valueOf(grade));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
