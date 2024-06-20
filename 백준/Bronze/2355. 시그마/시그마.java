3e10073037675703799b852aa724abf9d1919326
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-20 11:22:12 +0900

    [Bronze II] Title: 시그마, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/2355. 시그마/시그마.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        long a = Math.min(N,M);
+        long b = Math.max(N,M);
+
+        long sigma = b*(b+1)/2;
+        long sub = (a-1)*a/2;
+        long ans = sigma - sub;
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
