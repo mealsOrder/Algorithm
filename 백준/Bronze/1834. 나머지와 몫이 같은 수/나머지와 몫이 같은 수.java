d281ec7130fdb920519e2a2a778ca6274c35922a
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-20 12:01:10 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 136 ms, Memory: 15040 KB -BaekjoonHub

---
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long ans=0;
+        long N = Integer.parseInt(br.readLine());
+        for(long i=1;i<N;i++){
+            ans += i*N+i;
+        }
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
