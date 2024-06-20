516463e285b577594be4b961b16097e7f01689e3
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-20 15:58:55 +0900

    [Bronze IV] Title: 모음의 개수, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int ans=0;
+        String str = br.readLine();
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c =='a' || c=='e' || c=='o' || c=='i' || c=='u'){
+                ans++;
+            }
+        }
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
