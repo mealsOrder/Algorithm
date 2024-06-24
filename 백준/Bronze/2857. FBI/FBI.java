9f990dd1e26805dbf7f01d0e1c63541146c423d1
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-24 14:22:36 +0900

    [Bronze III] Title: FBI, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2857. FBI/FBI.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String[] arr = new String[5];
+        boolean flag = false;
+        for(int i=0;i<5;i++){
+            arr[i] = br.readLine();
+            if(arr[i].contains("FBI")){
+                sb.append(i+1).append(" ");
+                flag = true;
+            }
+        }
+        if(flag) System.out.println(sb);
+        else System.out.println("HE GOT AWAY!");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
