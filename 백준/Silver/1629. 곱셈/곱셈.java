fe8214ffae301c0acfa075c251fce13424cd1002
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-25 21:19:55 +0900

    [Silver I] Title: 곱셈, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        bw.write(String.valueOf(func(A,B,C)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static long func(long a,long b,long c){
+        if(b==1) return a%c;
+
+        long model = func(a,b/2,c);
+        // 홀수일때
+        if(b%2==1) return (model*model%c) * a%c;
+        //짝수일때
+        return (model*model)%c;
+    }
+
+}
