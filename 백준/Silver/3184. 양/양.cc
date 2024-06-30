9caf9395e819ee0273314cfeaa52f614ba8fa141
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-06-30 20:59:07 +0900

    [Silver I] Title: 양, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.cc
+++ 백준/Silver/3184. 양/양.cc
@@ -2,23 +2,21 @@
 #include<vector>
 #include<algorithm>
 #include<queue>
-#include<set>
-#include<deque>
 using namespace std;
 const int MX = 251;
+int R,C;
 string map[MX];
 bool vis[MX][MX];
-int R,C;
 int dx[]={0,0,1,-1};
 int dy[]={1,-1,0,0};
 int ans_o,ans_v;
 void BFS(int x,int y){
     queue<pair<int,int>>q;
-    int v=0,o=0;
+    int o=0,v=0;
     vis[x][y]=true;
     q.push({x,y});
-    if(map[x][y] == 'v')v++;
-    else if(map[x][y] == 'o')o++;
+    if(map[x][y] == 'o')o++;
+    else if(map[x][y] == 'v')v++;
 
     while(!q.empty()){
         int curX = q.front().first;
@@ -30,31 +28,32 @@ void BFS(int x,int y){
             int ny = curY+dy[i];
 
             if(nx>=0&&nx<R&&ny>=0&&ny<C&&!vis[nx][ny]&&map[nx][ny]!='#'){
+                if(map[nx][ny] == 'o') o++;
+                else if(map[nx][ny] == 'v')v++;
                 vis[nx][ny] = true;
-                q.push({ nx, ny });
-                if (map[nx][ny] == 'v') v++;
-                else if (map[nx][ny] == 'o') o++;
+                q.push({nx,ny});
             }
         }
     }
-    if (o > v) ans_o += o;
-    else ans_v += v;
+    if(v>=o)ans_v+=v;
+    else ans_o+=o;
 }
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    cin >> R >>C;
+    cin >> R>>C;
+
     for(int i=0;i<R;i++){
         cin >> map[i];
     }
 
     for(int i=0;i<R;i++){
         for(int j=0;j<C;j++){
-            if(map[i][j]!='#' && !vis[i][j]){
+            if(map[i][j] != '#' &&!vis[i][j]){
                 BFS(i,j);
             }
         }
     }
-    cout << ans_o << " " << ans_v;    
+    cout << ans_o << " " << ans_v << '\n';
     return 0;
 }
