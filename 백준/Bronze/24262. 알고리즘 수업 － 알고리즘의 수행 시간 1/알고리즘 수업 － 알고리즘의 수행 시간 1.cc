2856952abeedefe40f9b3e80d2e047b6e7c3500f
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-10-28 22:34:25 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int N;
+    cin >> N;
+    cout << 1 << '\n' << 0;
+    return 0;
+}
