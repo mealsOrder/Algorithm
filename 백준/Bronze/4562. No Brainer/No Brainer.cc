8f48ea990cf06e78e9a9e5b14221fed6a9d9a3c4
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-12-10 19:57:43 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		if (a < b) { cout << "NO BRAINS\n"; }
+		else { cout << "MMM BRAINS\n"; }
+	}
+}
