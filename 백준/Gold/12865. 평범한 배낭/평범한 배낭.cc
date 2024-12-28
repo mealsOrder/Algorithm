933a92acf631308389a20d435425844ffe884879
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2024-12-29 00:28:24 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,31 +1,35 @@
 #include<iostream>
 #include<algorithm>
-#include<cmath>
+#include<vector>
 using namespace std;
-const int MX = 101;
-int dp[MX][100001];
-int V[MX];
-int W[MX];
 
+const int MX = 101;
+int N,K;
+int dp[MX][100001];
+int W[MX];
+int V[MX];
 int main(){
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    int N,K;
+
     cin >> N >> K;
 
     for(int i=1;i<=N;i++){
-        cin>> W[i] >> V[i];
+        cin >> W[i] >> V[i];
     }
-
+    
     for(int i=1;i<=N;i++){
-        for(int j=1;j<=K;j++){
-            if(W[i]>j)dp[i][j] = dp[i-1][j];
+        for(int j=0;j<=K;j++){
+            // 최대 무게 보다 현재 무게가 클 경우
+            if(W[i] > j){
+                dp[i][j] = dp[i-1][j];
+            }
             else{
-                dp[i][j] = max(dp[i-1][j],dp[i-1][j-W[i]]+V[i]);
+                dp[i][j] = max( dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
             }
         }
     }
 
-    cout << dp[N][K]<< '\n';
+    cout << dp[N][K];
     return 0;
 }
