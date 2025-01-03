286f4bc9089a435d686d96bb74dc2341e096140b
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2025-01-03 23:11:54 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int N;
+long long ans=1;
+int main()
+{
+	cin >> N;
+    cout << (1<< N);
+	return 0;
+}
