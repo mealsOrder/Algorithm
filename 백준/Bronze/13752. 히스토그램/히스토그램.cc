6ec19ce69e18ae0ade3099fba40829a804bfaf8d
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2025-01-07 23:36:10 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		for (int j = 0; j < a; j++) {
+			cout << "=";
+		}
+		cout << '\n';
+	}
+}
