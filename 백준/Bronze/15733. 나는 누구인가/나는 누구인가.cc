b2ebb1b2ba1a56159508b7c49d46a4f43054f640
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2025-04-22 22:09:02 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX 10001
+#define INF 987654321
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    
+    cout << "I'm Sexy";
+    return 0;
+}
