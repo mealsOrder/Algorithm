7ba9e58312396847d26f38caf10b38d148e5e0c7
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2025-05-05 14:05:26 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4532 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -9,25 +9,26 @@ using namespace std;
 
 const int MX = 501;
 
+int N,M;
 int map[MX][MX];
+int dp[MX][MX];
 int dx[] = {0,0,1,-1};
 int dy[] = {1,-1,0,0};
-int dp[MX][MX];
-
-int N,M;
 
 int DFS(int x,int y){
-    if(x == N-1 && y == M-1) return 1;
-
+    // 종료 조건
+    if( x == N-1 && y == M-1) return 1;
     if(dp[x][y] != -1) return dp[x][y];
 
     dp[x][y] = 0;
 
     for(int i=0;i<4;i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+        int nx = x+dx[i];
+        int ny = y+dy[i];
 
         if(nx>=0&&nx<N&&ny>=0&&ny<M&& map[x][y] > map[nx][ny]){
+            // dp[nx][ny] += DFS(nx,ny) 가 아닌 이유
+            // 현재 (x,y) 기준으로 다음 경로 수를 누적해야함
             dp[x][y] += DFS(nx,ny);
         }
     }
@@ -47,6 +48,7 @@ int main(){
     }
 
     memset(dp,-1,sizeof(dp));
+
     cout << DFS(0,0) << '\n';
     return 0;
 }
