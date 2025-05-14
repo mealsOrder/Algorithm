fbd21be926b324ac7542c08ef7143e8c9d340b95
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2025-05-14 19:17:18 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 12 ms, Memory: 7024 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,37 +1,38 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<algorithm>
 #include<queue>
+#include<climits>
+#include<cstring>
 using namespace std;
 
 const int MX = 1001;
-
-int a[MX][MX];
+string str[MX];
 int dp[MX][MX];
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
     int N,M;
     cin >> N >> M;
 
-    for(int i=0;i<N;i++){
-        for(int j=0;j<M;j++){
-            char c;
-            cin >> c;
-            a[i][j] = c - '0';
-        }
-    }
+    for(int i=0;i<N;i++)cin >> str[i];
 
     int ans = 0;
     for(int i=0;i<N;i++){
         for(int j=0;j<M;j++){
-            dp[i][j] = a[i][j];
-            if( a[i][j] && i>0 && j>0){
-                dp[i][j] = min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1])) + 1;
+            if(str[i][j] == '1'){
+                if(i == 0 || j==0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    dp[i][j] = min ( {dp[i-1][j] , dp[i][j-1] , dp[i-1][j-1]}) +1;
+                }
+                ans = max(ans,dp[i][j]);
             }
-            ans = max(ans,dp[i][j]);
         }
     }
-    cout << ans * ans <<'\n';
+    cout << ans * ans << '\n';
+
     return 0;
 }
