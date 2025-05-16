6aec224d67834efc8ac3ca28411e41bffba9b235
mealsOrder <44600477+mealsOrder@users.noreply.github.com>
2025-05-16 09:09:55 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 36 ms, Memory: 19480 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -1,30 +1,25 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<tuple>
-#include<cstring>
-#include<queue>
-#include<climits>
 using namespace std;
 
 const int MX = 501;
 
 int N;
-int map[MX][MX];
+int board[MX][MX];
 int dp[MX][MX];
-int dx[]={0,0,1,-1};
-int dy[]={1,-1,0,0};
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
 
 int DFS(int x,int y){
-    if(dp[x][y]) return dp[x][y];
+    if(dp[x][y])return dp[x][y];
     dp[x][y]=1;
 
     for(int i=0;i<4;i++){
-        int nx = x+dx[i];
-        int ny = y+dy[i];
+        int nx=x+dx[i];
+        int ny=y+dy[i];
 
-        if(nx>=0&&nx<N&&ny>=0&&ny<N&& map[x][y] < map[nx][ny]){
-            
+        if(nx>=0&&nx<N&&ny>=0&&ny<N&&board[x][y]<board[nx][ny]){
             dp[x][y] = max(dp[x][y], DFS(nx,ny)+1);
         }
     }
@@ -32,26 +27,22 @@ int DFS(int x,int y){
 }
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    cin >> N;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
+    cin >> N;
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            cin >> map[i][j];
+            cin >> board[i][j];
         }
     }
-    
     int ans = 0;
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
-            int a = DFS(i,j);           
+            int a = DFS(i,j);
             ans = max(ans,a);
-            //cout << a << ' ' << i << ' ' << j <<'\n';
         }
     }
-
     cout << ans << '\n';
-
     return 0;
 }
